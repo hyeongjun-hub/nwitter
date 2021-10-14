1f659c5bc4c8a02b9b454b42cb23b1195abbbff5
hyeongjun <chogudwns@naver.com>
2021-10-14 20:52:06 +0900

    login_complete

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,18 @@
 import React, { useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+import { authService } from "fbase";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
   const onChange = (e) => {
     const {
       target: { name, value },
@@ -13,12 +23,52 @@ const Auth = () => {
       setPassword(value);
     }
   };
-  const onSubmint = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        data = await createUserWithEmailAndPassword(
+          authService,
+          email,
+          password
+        );
+      } else {
+        data = await signInWithEmailAndPassword(authService, email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      setError(error.message);
+    }
   };
+  const toggleAccount = () => setNewAccount((prev) => !prev);
+  const onSocialClick = async (e) => {
+    const {
+      target: { name },
+    } = e;
+    let provider;
+    if (name === "google") {
+      provider = new GoogleAuthProvider();
+      signInWithPopup(authService, provider)
+        .then((result) => {
+          //   const credential = GoogleAuthProvider.credentialFromResult(result);
+          //   const token = credential.accessToken;
+          //   const user = result.user;
+        })
+        .catch((error) => {
+          console.log(error.message);
+        });
+    } else if (name === "github") {
+      provider = new GithubAuthProvider();
+      await signInWithPopup(authService, provider).catch((error) => {
+        console.log(error.message);
+      });
+    }
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={onSubmit}>
         <input
           name="email"
           type="text"
@@ -35,11 +85,19 @@ const Auth = () => {
           value={password}
           onChange={onChange}
         />
-        <input name="password" type="submit" value="login" />
+        <input type="submit" value={newAccount ? "createAccount" : "login"} />
+        {error}
       </form>
+      <span onClick={toggleAccount}>
+        {newAccount ? "Sign In" : "Create Account"}
+      </span>
       <div>
-        <button>Continue with Google</button>
-        <button>Continue with Github</button>
+        <button name="google" onClick={onSocialClick}>
+          Continue with Google
+        </button>
+        <button name="github" onClick={onSocialClick}>
+          Continue with Github
+        </button>
       </div>
     </div>
   );
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,23 @@
 import React from "react";
+import { signOut } from "firebase/auth";
+import { authService } from "fbase";
+import { useHistory } from "react-router";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const history = useHistory();
+  const logOutClick = async (e) => {
+    try {
+      await signOut(authService);
+    } catch (error) {
+      console.log(error.message);
+    }
+    history.push("/");
+  };
+  return (
+    <>
+      <span>Home</span>
+      <button onClick={logOutClick}>logout</button>
+    </>
+  );
+};
 export default Home;
