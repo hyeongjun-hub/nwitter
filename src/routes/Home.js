65e248f70f41ca3c72b2a61cc0d206acd5c03269
hyeongjun <chogudwns@naver.com>
2021-11-01 12:28:00 +0900

    db_connect

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { signOut } from "firebase/auth";
-import { authService } from "fbase";
+import { authService, dbService } from "fbase";
 import { useHistory } from "react-router";
+import { collection, addDoc, getDocs } from "firebase/firestore";
 
 const Home = () => {
   const history = useHistory();
+  const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+  const getNweets = async () => {
+    try {
+      const dbNweets = await getDocs(collection(dbService, "nweets"));
+      dbNweets.forEach((doc) => {
+        const nweetObject = {
+          ...doc.data(),
+          id: doc.id,
+        };
+        setNweets((prev) => [nweetObject, ...prev]);
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  useEffect(() => {
+    getNweets();
+  }, []);
   const logOutClick = async (e) => {
     try {
       await signOut(authService);
@@ -13,10 +33,46 @@ const Home = () => {
     }
     history.push("/");
   };
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await addDoc(collection(dbService, "nweets"), {
+        text: nweet,
+        createdAt: Date.now(),
+      });
+      setNweet("");
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNweet(value);
+  };
+  console.log(nweets);
   return (
     <>
       <span>Home</span>
       <button onClick={logOutClick}>logout</button>
+      <form onSubmit={onSubmit}>
+        <input
+          value={nweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength="120"
+        />
+        <input type="submit" value="Nweet" />
+      </form>
+      <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h4>{nweet.nweet}</h4>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
